afc79fc194266f96ecae20d3bd3a4a4ccdbc0366
Misterxof <mishail.lukushevich@gmail.com>
2017-09-26 10:55:19 +0300

    update

--- app/src/main/java/com/mistcorp/tabsandswipe/MainActivity.java
+++ app/src/main/java/com/mistcorp/tabsandswipe/MainActivity.java
@@ -266,11 +266,11 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return getResources().getString(R.string.title_section1);
                 case 1:
-                    return "SECTION 2";
+                    return getResources().getString(R.string.title_section2);
                 case 2:
-                    return "SECTION 3";
+                    return getResources().getString(R.string.title_section3);
             }
             return null;
         }
